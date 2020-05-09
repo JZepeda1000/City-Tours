aecabbfbd8529c1a1a533fb4a0314ad0cad10737
JZepeda1000 <jz1043@yahoo.com>
2020-05-09 08:45:47 -0500

    Tour component

--- city-tours-app/src/components/TourList/TourList.js
+++ city-tours-app/src/components/TourList/TourList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Tour from '../Tour/Tour';
+import './tourlist.scss';
+import {tourData} from '../../tourData';
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData
+    };
+
+    removeTour = id  => {
+
+    }
+
     render() {
+
+        const { tours } = this.state;
+
         return (
-            <div>
-                Hello from TourList
-                <Tour />
-            </div>
+            <section className="tourlist">
+                {
+                tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>))
+                }
+            </section>
         )
     }
 };
